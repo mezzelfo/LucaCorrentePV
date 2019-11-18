ee1a207f913c20bad8cc3e29364c8185e03ddfcf
Luca Savant <luca.mezzelfo@gmail.com>
2019-11-18 22:29:09 +0100

    Versione Luca
    Controllo BANDA e SCARTO da Blynk

--- LucaCorrentePV.ino
+++ LucaCorrentePV.ino
@@ -1,167 +1,167 @@
 //#define TEL_SER Serial
 #define TEL_SER TelnetStream
 
-// Blynk defines
-#define BLYNk_HEARTBEAT 50
-#define BLYNK_PRINT Serial
-
 // Libraries Required
 #include <BlynkSimpleEsp8266.h>   //Blynk magic
 #include <ESP8266httpUpdate.h>    //For OTA Update
-#include <Filters.h>              //Analyze signal
+#include <Filters.h>              //For signal analysis
 #include <TelnetStream.h>         //Telent Debug
 
 // Blynk connection variables
-char auth[] = "OUbg10Z2bMiggS-7ZkJtvSEg7I1FMqJi";
+char auth[] = "3Ces1f8NBl4PrGAqDKKDddCIrrNwWPb7";
 char server[] = "lucanet.ddns.net";
 char ssid[] = "WebCube42-AB50";
 char pass[] = "ED248443";
 
-
-float Gain = 20.0; //19.5;
-float  Offset = 40.0; // Watt
-double RMS;
+// Global Variables
+RunningStatistics inputStats;
 BlynkTimer timer;
-float testFrequency = 50;                     // test signal frequency (Hz)
-float windowLength = 40.0 / testFrequency;   // how long to average the signal, for statistist
-RunningStatistics inputStats;                //Easy life lines, actual calculation of the RMS requires a load of coding
-
-
-float integrale = 0.0;
+WidgetTerminal terminal(V0);
+double integraleWattSecondo;
 unsigned long tempovecchio;
 
-// This function will run every time Blynk connection is established
+float banda;
+int scarto;
+
 BLYNK_CONNECTED() {
-  //get data stored in virtual pin V0 from server
-  Blynk.syncVirtual(V30);
+  Blynk.syncVirtual(V1);
+  Blynk.syncVirtual(V50);
+  Blynk.syncVirtual(V51);
 }
 
-// restoring counter from server
-BLYNK_WRITE(V30)
+BLYNK_WRITE(V1)
 {
-  //restoring int value
-  integrale = param.asFloat();
+  integraleWattSecondo = param.asDouble();
+}
+
+BLYNK_WRITE(V50)
+{
+  banda = param.asInt()/100.0;
+}
+
+BLYNK_WRITE(V51)
+{
+  scarto = param.asInt();
 }
 
 void fun()
 {
   int sensormin = 1025;
   int sensormax = -1;
-
   int voltetransizioni = 0;
-
   int stato, statoold = false;
-
-  long starttime, deltatime;
+  unsigned long starttime, deltatime;
   int counter = 0;
+  int counterold = 0;
   double sensormean = 0;
 
   while (voltetransizioni < 100 + 5)
   {
-
     counter++;
     if (counter >= 4000)
       break;
     int sensor = analogRead(A0);
-
     if (sensor < sensormin) sensormin = sensor;
     if (sensor > sensormax) sensormax = sensor;
     inputStats.input(sensor);
-
     sensormean = sensormean * (counter - 1) / counter + sensor * 1.0 / counter;
-
-    if (sensor > sensormean * (2.0 / 3.0) + sensormax * (1.0 / 3.0))
+    if (sensor > sensormean * (1.0-banda) + sensormax * (banda))
       stato = true;
-    if (sensor < sensormean * (2.0 / 3.0) + sensormin * (1.0 / 3.0))
+    if (sensor < sensormean * (1.0-banda) + sensormin * (banda))
       stato = false;
-
-    if (stato != statoold)
+    if ((stato != statoold) && (counter > counterold + scarto))
     {
-      //yield();
+      counterold = counter;
       voltetransizioni++;
       if (voltetransizioni == 5) starttime = micros();
     }
-
     statoold = stato;
-
-
   }
 
   deltatime = micros() - starttime;
-  float freq = (10000000000.0 / deltatime) / 2;
-  RMS = Gain * inputStats.sigma() - Offset;
-  integrale += RMS * float(millis() - tempovecchio);
+  double freq = (10000000000.0 / deltatime) / 2.0;
+  
+  double RMS = 20.0 * inputStats.sigma() - 0.0;
+  integraleWattSecondo += RMS * (millis() - tempovecchio) / 1000.0;
   tempovecchio = millis();
 
-  TEL_SER.print("Intg: ");
-  TEL_SER.print(integrale * 1.0 / 3600000000.0, 4);
-  TEL_SER.print("\t");
+  TEL_SER.print("Intg: "+String(integraleWattSecondo/3600000.0,4)+"\t");
 
-  TEL_SER.print("Freq: ");
-  TEL_SER.print(freq);
-  TEL_SER.print("\t");
+  TEL_SER.print("Freq: "+String(freq)+"\t");
 
-  TEL_SER.print("Tran: ");
-  TEL_SER.print(voltetransizioni);
-  TEL_SER.print("\t");
-  TEL_SER.print(counter);
-  TEL_SER.print("\t");
+  TEL_SER.print("Tran: "+String(voltetransizioni)+"\t"+String(counter)+"\t");
 
-  TEL_SER.print("Mean: ");
-  TEL_SER.print(sensormean);
-  TEL_SER.print("\t");
+  TEL_SER.print("Mean: "+String(sensormean)+"\t");
 
-  //TEL_SER.print("time: ");
-  //TEL_SER.print(deltatime * 1.0 / 1000.0);
-  //TEL_SER.print("\t");
+  //TEL_SER.print("time: "+String(deltatime / 1000.0)+"\t");
 
-  TEL_SER.print("sMin / SMax : ");
-  TEL_SER.print(sensormin);
-  TEL_SER.print(" ");
-  TEL_SER.print(sensormax);
-  TEL_SER.print("\t");
+  TEL_SER.print("sMin / SMax : "+String(sensormin)+" "+String(sensormax)+"\t");
 
-  TEL_SER.print("Sigm: ");
-  TEL_SER.print(RMS);
+  TEL_SER.print("Sigm: "+String(RMS));
   TEL_SER.println();
+  
+  Blynk.virtualWrite(V1, integraleWattSecondo);
+  Blynk.virtualWrite(V2, integraleWattSecondo/3600000.0); //IntegraleKiloWattOra
+  Blynk.virtualWrite(V3, sensormin);
+  Blynk.virtualWrite(V4, sensormax);
+  Blynk.virtualWrite(V5, RMS); //Watt
+  Blynk.virtualWrite(V6, freq);
+}
 
-  if (counter < 4000) Blynk.virtualWrite(V10, freq);
-  Blynk.virtualWrite(V11, RMS);
-  Blynk.virtualWrite(V20, sensormin);
-  Blynk.virtualWrite(V21, sensormax);
-  Blynk.virtualWrite(V30, integrale * 1.0 / 3600000000.0);
-
+BLYNK_WRITE(V0)
+{
+  if (String("R") == param.asStr()) {
+    terminal.println("Rebooting");
+    terminal.flush();
+    delay(100);
+    ESP.restart();
+  }
+  else if (String("C") == param.asStr()) {
+    Blynk.virtualWrite(V1,0);
+    terminal.println("Clearing integraleWattSecondo");
+    terminal.flush();
+    delay(100);
+    ESP.restart();
+  }
+  else {
+    terminal.print("Command not found");
+    terminal.flush();
+  }  
 }
 
 void setup()
 {
-  pinMode(5, INPUT);
-  pinMode(A0, INPUT);
-
-  inputStats.setWindowSecs(windowLength);
+  integraleWattSecondo = 0.0;
+  inputStats.setWindowSecs(40.0/50.0);
   tempovecchio = millis();
+  pinMode(5, INPUT);
+  pinMode(A0, INPUT);  
 
-  Serial.begin(9600);
   TelnetStream.begin();
-
   Blynk.begin(auth, ssid, pass, server, 8080);
   timer.setInterval(2000L, fun);
-  //Blynk.syncAll();
-  Blynk.syncVirtual(V30);
-
+  terminal.clear();
+  terminal.println("Device started");
+  terminal.println("-------------");
+  terminal.flush();
 }
 
 void loop()
 {
   Blynk.run();
   timer.run();
-
+  
   if (TelnetStream.read() == 'R') {
     TelnetStream.stop();
     delay(100);
     ESP.restart();
   }
-  
+  if (TelnetStream.read() == 'C') {
+    Blynk.virtualWrite(V1,0);
+    TelnetStream.stop();
+    delay(100);
+    ESP.restart();
+  }
 }
 
 BLYNK_WRITE(InternalPinOTA) {
