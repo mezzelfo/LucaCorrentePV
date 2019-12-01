685d9d130001c74fe8c96ff2b1919c47c6ee5519
Luca Savant <luca.mezzelfo@gmail.com>
2019-12-01 13:37:17 +0100

    Update flasToSanfra.sh
    Token corretto

--- flasToSanfra.sh
+++ flasToSanfra.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -v -F file=@LucaCorrentePV.ino.d1.bin --insecure -u admin@blynk.cc:B3ppon3123456 https://lucanet.ddns.net:9443/admin/ota/start?token=OUbg10Z2bMiggS-7ZkJtvSEg7I1FMqJi
+curl -v -F file=@LucaCorrentePV.ino.d1.bin --insecure -u admin@blynk.cc:B3ppon3123456 https://lucanet.ddns.net:9443/admin/ota/start?token=3Ces1f8NBl4PrGAqDKKDddCIrrNwWPb7
